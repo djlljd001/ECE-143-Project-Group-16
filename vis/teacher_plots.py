145437af8689cb8291fbc98cb1b823032202e275
Shreyas Rajesh <shreyasrajesh@shreyass-mbp-2.home>
2020-03-13 20:48:51 -0700

    More teacher plots

--- vis/teacher_plots.py
+++ vis/teacher_plots.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from src.preprocess import *
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 # Load the data into the school and student variables respectively.
 sc, st = clean()
@@ -30,6 +31,54 @@ def affirmative_attitude(st):
     sns.set(style="darkgrid")
     plt.rcParams["figure.figsize"] = (25,25)
     sns.lineplot(y='X1SCIEFF', x='AFFATT',data=df)
+
+
+def linear_regression_affatt(st):
+    '''
+    This method performs a linear regression analysis of the teacher's
+    attitude variables against student's science self-efficacy
+    showing us that affirmative attitude is the largest influencer of
+    student science self-efficacy.
+    :param st: Student variables dataframe object
+    '''
+    assert isinstance(st, pd.DataFrame)
+    # Linear regression only on teacher variables
+    plt.rcParams["figure.figsize"] = (10,10)
+    df = st[['X1SEX','N1INTEREST','N1CONCEPTS','N1TEST','N1PREPARE','N1IDEAS','N1SEX','X1TSCERT','X1SCIEFF','N1GROUP','S1STCHVALUES','S1STCHRESPCT','S1STCHFAIR','S1STCHCONF',
+                       'S1STCHMISTKE','S1STCHTREAT','S1STCHMFDIFF']]
+
+    df = df[df['X1SCIEFF'] != -2.91]
+    df['S1STCHTREAT'] = st['S1STCHTREAT'].apply(lambda x: 5-x)
+    df['S1STCHMFDIFF'] = st['S1STCHMFDIFF'].apply(lambda x: 5-x)
+
+    df['AFFATT'] = df[['S1STCHVALUES','S1STCHRESPCT','S1STCHFAIR','S1STCHCONF',
+                       'S1STCHMISTKE','S1STCHTREAT','S1STCHMFDIFF']].sum(axis=1)/7.0
+    df['AFFATT'] = df['AFFATT'].apply(lambda x: 5-x)
+    final_df = df[['N1INTEREST','N1TEST','N1CONCEPTS','N1PREPARE','N1IDEAS','N1SEX','X1TSCERT','N1GROUP','AFFATT']]
+    final_df = final_df.astype('category')
+
+    model = LinearRegression()
+    final_df.head()
+    model.fit(final_df, df['X1SCIEFF'])
+    model.score(final_df, df['X1SCIEFF'])
+    Y_pred = model.predict(final_df)
+    coefs = dict(zip(final_df.columns,model.coef_))
+    for key, val in coefs.items():
+        print('{} : {}'.format(key, val))
+    # model.coef_
+    #print(type(model.coef_))
+    #print(coefs.keys)
+
+    # Plotting
+    colors=["#5DADE2"]
+    sns.set_style("whitegrid")
+    #sns.set_style("ticks", {"xtick.major.size": 8, "ytick.major.size": 8})
+    sns.set_palette(sns.color_palette(colors))
+    plt.barh(list(coefs.keys()), list(coefs.values()))
+    plt.yticks([x for x in range(9)],['Interest', 'Preparation', 'Concepts', 'Certification', 'Teacher\'s gender',\
+                                      'Test Quality', 'Affirmative attitude', 'New Ideas', 'Group Work'],fontsize='xx-large')
+    plt.xlim([-0.1,0.4])
+    plt.xlabel('Correlation with Science Self-Efficacy',fontsize='xx-large')
 
 
 def affatt_boxplots(st):
@@ -104,5 +153,7 @@ if __name__ == "__main__":
     sc, st = clean(args.school_file, args.student_file)
 
     # Comment based on desired plots. Leave as is for all plots.
+    linear_regression_affatt(st)
+    affatt_boxplots(st)
     affirmative_attitude(st)
     gender_certification(st)
