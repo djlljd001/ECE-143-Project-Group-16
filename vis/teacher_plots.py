ab94e7cdd0962bbee2c12629deddaaf66c5fc341
SubratoChakravorty <chakravorty.subrato@gmail.com>
2020-03-13 23:57:33 -0700

    analysis

--- vis/teacher_plots.py
+++ vis/teacher_plots.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from scipy import stats
+
 
 def teacher_gender(st):
     '''
